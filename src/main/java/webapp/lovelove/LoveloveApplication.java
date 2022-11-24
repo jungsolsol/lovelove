54daf3cfb58400c3f7c1368195e8c8ab326eb8f8
jungsolsol <jsol78@naver.com>
2022-11-25 04:46:33 +0900

    finally23

--- src/main/java/webapp/lovelove/LoveloveApplication.java
+++ src/main/java/webapp/lovelove/LoveloveApplication.java
@@ -3,12 +3,18 @@ package webapp.lovelove;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude={MultipartAutoConfiguration.class})
 public class LoveloveApplication  {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "optional:classpath:application-oauth.yml,"
+			+ "optional:/usr/local/myapp/application-oauth.yml";
 	public static void main(String[] args) {
-		SpringApplication.run(LoveloveApplication.class, args);
+
+		new SpringApplicationBuilder(LoveloveApplication.class).properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
