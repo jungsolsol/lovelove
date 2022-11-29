f5bbfdb552b1674b5e2c084dfb246201dd2e5dce
jungsolsol <jsol78@naver.com>
2022-11-30 01:33:23 +0900

    finally23

--- src/main/java/webapp/lovelove/config/SecurityConfig.java
+++ src/main/java/webapp/lovelove/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
@@ -39,16 +40,15 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/")
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/auth/login")
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/login")
+                .loginPage("/auth/login")
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService);
         return http.build();
     }
-
 
 }
 
