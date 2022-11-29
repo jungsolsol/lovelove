3c4f11f61872500d38b7b982b01e530116cd42e0
jungsolsol <jsol78@naver.com>
2022-11-29 20:39:02 +0900

    finally23

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
-
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
@@ -16,10 +16,10 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 #nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 애플리케이션 배포!! "
 JAR_NAME=$(ls -tr $PROJECT_ROOT/ | grep jar | tail -n 1)
-echo "> JAR Name: $JAR_NAME"
 
+echo "> JAR Name: $JAR_NAME"
 nohup java -jar \
 -Dspring.config.location=classpath:/application-oauth.yml,/home/ubuntu/app/src/main/resources/application-oauth.properties \
-   $PROJECT_ROOT/$JAR_NAME 2>&1 &
+  $PROJECT_ROOT/$JAR_NAME 2>&1 &
